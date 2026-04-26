8335f3523b1dda8d7fe68930800ded1d9d26584d
Francisco <liz066@ucsd.edu>
2026-04-25 21:44:01 -0700

    Lab 4: add fetchGitHubData to global.js, update index.js

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-import { fetchJSON, renderProjects } from './global.js';
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 
 const projects = await fetchJSON('./lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects.slice(0, 3), projectsContainer, 'h2');
-const githubData = await fetchJSON('https://api.github.com/users/LiuxuhongDS');
+
+const githubData = await fetchGitHubData('LiuxuhongDS');
 
 const profileStats = document.querySelector('#profile-stats');
-profileStats.innerHTML = `
-  <dl>
-    <dt>Public Repos</dt><dd>${githubData.public_repos}</dd>
-    <dt>Followers</dt>  <dd>${githubData.followers}</dd>
-    <dt>Following</dt>  <dd>${githubData.following}</dd>
-  </dl>
-`;
+if (profileStats) {
+  profileStats.innerHTML = `
+    <dl>
+      <dt>Public Repos</dt><dd>${githubData.public_repos}</dd>
+      <dt>Public Gists</dt><dd>${githubData.public_gists}</dd>
+      <dt>Followers</dt><dd>${githubData.followers}</dd>
+      <dt>Following</dt><dd>${githubData.following}</dd>
+    </dl>
+  `;
+}
